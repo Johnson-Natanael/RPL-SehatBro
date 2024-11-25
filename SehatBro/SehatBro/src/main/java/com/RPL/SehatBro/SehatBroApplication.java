e742a2a3c01e2a45f554a908faf734a128ee038c
johnson natanael <johnsonnatanael@gmail.com>
2024-11-25 18:46:30 +0700

    First commist add project and Folderquery

---
+++ SehatBro/SehatBro/src/main/java/com/RPL/SehatBro/SehatBroApplication.java
@@ -0,0 +1,13 @@
+package com.RPL.SehatBro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SehatBroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SehatBroApplication.class, args);
+	}
+
+}
---
+++ SehatBro/SehatBro/src/test/java/com/RPL/SehatBro/SehatBroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RPL.SehatBro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SehatBroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
